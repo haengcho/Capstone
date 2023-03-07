6a4bca239efe8315830cd0cf2efc17b8d1faec1a
HAENGCHO\acctr <acctract@naver.com>
2023-03-08 01:02:42 +0900

    로그인, 회원가입 레이아웃 -> 키워드 선택 미완 따로 만들기

--- Capstone/app/src/main/java/com/haengcho/capstone/JoinActivity.kt
+++ Capstone/app/src/main/java/com/haengcho/capstone/JoinActivity.kt
@@ -4,6 +4,8 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
@@ -19,6 +21,19 @@ class JoinActivity : AppCompatActivity() {
         joinBtn.setOnClickListener {
             val intent = Intent(this, JoinMemberShipActivity::class.java)
             startActivity(intent)
+        }
+
+        val loginBtn = findViewById<Button>(R.id.loginBtn)
+        val idArea = findViewById<EditText>(R.id.idArea)
+        val passwordArea = findViewById<EditText>(R.id.passwordArea)
+        loginBtn.setOnClickListener {
+            if(idArea.text.isEmpty() || passwordArea.text.isEmpty()){
+            Toast.makeText(this, "아이디와 비밀번호를 다시 확인해주세요.", Toast.LENGTH_LONG).show()
+        }
+            else{
+
+            }
+
         }
 
     }
--- Capstone/app/src/main/java/com/haengcho/capstone/JoinMemberShipActivity.kt
+++ Capstone/app/src/main/java/com/haengcho/capstone/JoinMemberShipActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 
@@ -17,15 +18,22 @@ class JoinMemberShipActivity : AppCompatActivity() {
 
         val nextBtn = findViewById<Button>(R.id.nextBtn)
         val checkArea = findViewById<CheckBox>(R.id.checkArea)
+        val nameArea = findViewById<EditText>(R.id.nameArea)
+        val emailArea = findViewById<EditText>(R.id.emailArea)
+        val id2Area = findViewById<EditText>(R.id.id2Area)
+        val password2Area = findViewById<EditText>(R.id.password2Area)
 
         //임시
         nextBtn.setOnClickListener {
-            if(checkArea.isChecked == true){
+            if(checkArea.isChecked == true && nameArea.text.isNotEmpty() && emailArea.text.isNotEmpty() && id2Area.text.isNotEmpty() && password2Area.text.isNotEmpty()){
                 val intent = Intent(this, KeywordActivity::class.java)
                 startActivity(intent)
             }
             else if(checkArea.isChecked == false){
                 Toast.makeText(this,"약관에 동의해 주세요.",Toast.LENGTH_LONG).show()
+            }
+            else if(nameArea.text.isEmpty() || emailArea.text.isEmpty() || id2Area.text.isEmpty() || password2Area.text.isEmpty()){
+                Toast.makeText(this,"필수 정보를 전부 입력해주세요.",Toast.LENGTH_LONG).show()
             }
 
         }
